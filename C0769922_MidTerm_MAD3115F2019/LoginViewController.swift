9b6b022f57a1c1f21f352ace1ed45444428b02f1
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-08 16:44:59 -0500

    3 Buttons Added

--- C0769922_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0769922_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var rememberSwitch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
