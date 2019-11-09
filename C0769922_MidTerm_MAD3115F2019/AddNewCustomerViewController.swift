7a5f2ecd043a1dd0d7692ca456f9a9c68e338c65
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-08 20:42:27 -0500

    protocol created

---
+++ C0769922_MidTerm_MAD3115F2019/AddNewCustomerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddNewCustomerViewController.swift
+//  C0769922_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-11-08.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+protocol AddNewCustomerViewControllerDelegate:NSObject {
+    func didSelectSaveBtn(_ name:String)
+}
